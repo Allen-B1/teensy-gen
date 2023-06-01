c41c9258704022c740a365d10543bb3e929a5059
Allen B <pingguo.ny@gmail.com>
2023-05-31 21:37:17 -0400

    initial commit

---
+++ gather.mjs
@@ -0,0 +1,28 @@
+import {writeFile, writeFileSync} from 'fs';
+import fetch from 'node-fetch';
+
+let roles = {};
+
+function sanitize(name) {
+    return name.toLowerCase().replace(/(\s|_|-)/g, "");
+}
+
+Promise.all(
+    [fetch("https://clocktower.patters.live/roles").then(data => data.json()).then(data => {
+        for (let item of data.data) {
+            roles[sanitize(item.id)] = roles[sanitize(item.id)] || {};
+            Object.assign(roles[sanitize(item.id)], {
+                name: item.name,
+                type: item.type,
+                ability: item.ability,
+            });
+        }
+    }),
+    fetch("https://script.bloodontheclocktower.com/data/roles.json").then(data => data.json()).then(data => {
+        for (let item of data) {
+            roles[sanitize(item.id)] = roles[sanitize(item.id)] || {};
+            roles[sanitize(item.id)].icon = "https://script.bloodontheclocktower.com/" + item.icon.slice(2);
+        }
+    })]).then(() => {
+        return new Promise((res) => writeFile("roles.json", JSON.stringify(roles), {}, res));
+    })
